bdf944dcbc9804bd37c34522dd9ab3615dc7a7a5
Taira Andreeva <taira.anndreeva@gmail.com>
2023-03-07 15:39:15 +0200

    Initial project version

---
+++ TransferBuses-master/src/index.js
@@ -0,0 +1,36 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "typeface-roboto";
+import "./i18n.js";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff5722",
+      dark: "#c41c00",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#607d8b",
+      dark: "#34515e",
+      contrastText: "#fff",
+    }
+  },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Suspense fallback={<div>loading...</div>}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
